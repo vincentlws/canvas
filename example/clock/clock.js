d1a376ce3be216a3f6b9c977b6fc4c348e73d050
vincent <vincentliangws@gmail.com>
2014-04-18 16:45:21 +0800

    完成时钟demo

--- example/clock/clock.js
+++ example/clock/clock.js
@@ -22,10 +22,11 @@
 		//创建圆
 		context.arc(CANVAS_WIDTH / 2, CANVAS_HEIGHT / 2, RADIUS, 0, Math.PI * 2, true);
 
-		//绘图
+		//画出圆形
 		context.stroke();
 	}
 
+	//绘画时刻数字
 	function drawNumerals() {
 		var numerals = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], //小时数组
 			angle = 0,
@@ -34,6 +35,7 @@
 			numeralWidth = 0; //字体宽度
 		
 		numerals.forEach(function(numeral) {
+			//计算出数字的角度
 			angle = Math.PI / 6 * (numeral - 3);
 
 			//检查字体宽度
@@ -46,35 +48,53 @@
 		})
 	}
 
-
+	//绘画中心圆点
 	function drawCenter() {
 		context.beginPath();
+
 		context.arc(CANVAS_WIDTH / 2, CANVAS_HEIGHT / 2, 5, 0, Math.PI * 2, true);
+
+		//填充中心圆点
 		context.fill();
 	}
 
+	/**
+	*	绘画指针（时分秒）
+	*	@参数 loc: 时间数
+	*		  isHour: 是否为小时
+	*/
 	function drawHand(loc, isHour) {
 		var angle = (Math.PI * 2) * (loc / 60) - Math.PI /2,
 			handRadius = isHour ? RADIUS - HAND_TRUNCATION - HOUR_HAND_TRUNCATION : RADIUS - HAND_TRUNCATION;
 
+		//利用moveTo与lineTo 画出指针
 		context.moveTo(CANVAS_WIDTH / 2, CANVAS_HEIGHT / 2);
+
 		context.lineTo(CANVAS_WIDTH / 2 + Math.cos(angle) * handRadius, CANVAS_HEIGHT / 2 + Math.sin(angle) * handRadius);
 
 		context.stroke();
 	}
 
+	//绘画出时分秒指针
 	function drawHands() {
 		var date = new Date(),
 			hour = date.getHours();
 
 		hour = hour > 12 ? hour - 12 : hour;
 
+		//画出时针
 		drawHand(hour * 5 + (date.getMinutes() / 60) * 5, true);
+
+		//画出分针
 		drawHand(date.getMinutes(), false);
+
+		//画出秒针
 		drawHand(date.getSeconds(), false);
 	}
 
+
 	function drawClock() {
+		//清除所有像素
 		context.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 
 		drawCircle();
